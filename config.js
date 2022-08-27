60e0876b38df618909d2f7237854d214f0a046ce
Sreshta1234 <104977341+Sreshta1234@users.noreply.github.com>
2022-08-27 15:05:22 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+ import * as firebase from "firebase";
+ 
+
+//initialize your database
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBf4SDZ5oyGdSb9z3g4w3y598R08lhd9H0",
+  authDomain: "school-attendance-app-499f6.firebaseapp.com",
+  projectId: "school-attendance-app-499f6",
+  storageBucket: "school-attendance-app-499f6.appspot.com",
+  messagingSenderId: "877765952790",
+  appId: "1:877765952790:web:32e7c6c7f1c287b9e9bc37",
+  measurementId: "G-508KBBKLF0"
+};
+  
+export function database() {
+throw new Error("Function not implemented.");
+}
